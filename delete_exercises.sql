8427bbaeefef568b3ee91c41975797fcc338239c
benny-fields3rd <benny.fields3rd@gmail.com>
2018-12-03 09:31:36 -0600

    updated delete_exercise file

--- delete_exercises.sql
+++ delete_exercises.sql
@@ -1,10 +1,12 @@
 use codeup_test_db;
 
+SELECT * FROM albums;
+
 SELECT 'albums released after 1991:';
-DELETE * from albums WHERE release_date > 1991;
+DELETE from albums WHERE release_date > 1991;
 
 SELECT 'albums with Disco genre';
-DELETE * from albums WHERE genre = 'Disco';
+DELETE from albums WHERE genre = 'Disco';
 
 SELECT 'albums with Whitney Houston';
-DELETE * from albums WHERE artist = 'Whitney Houston';
+DELETE from albums WHERE artist = 'Whitney Houston';
